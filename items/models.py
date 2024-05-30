c83683d072a8ea0b1c76f59e7eddaf9ace8b87b3
Simon Oxford <simonoxford99@gmail.com>
2024-05-30 18:01:47 +0100

    Added owner field in user and item

--- items/models.py
+++ items/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 from django.conf import settings
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+User = get_user_model()
+
 
 class Item(models.Model):
 
@@ -11,16 +15,16 @@ class Item(models.Model):
     colour = models.CharField(max_length=100)
     year_of_release = models.PositiveIntegerField()
     size = models.CharField(max_length=2)
-    # owner = models.ForeignKey(to='users.User',related_name='items_created', on_delete=models.SET_NULL, null=True,blank=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='items')
     price = models.DecimalField(decimal_places=2, max_digits=5)
     description = models.CharField(max_length=250, default='', blank=True, null=True)
 
     def __str__(self):
-      return f"{self.brand} {self.type}"
+      return f"{self.brand} {self.type} ({self.colour})"
 
 
     # categories = models.ForeignKey ("category.Category", on_delete=models.CASCADE)
 
-    
+
 
 
