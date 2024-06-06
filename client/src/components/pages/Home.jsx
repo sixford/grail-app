9f58d046fa4e9db1e62f200190b91fdff05b446e
Simon Oxford <simonoxford99@gmail.com>
2024-06-06 08:51:09 +0100

    Working scroll banner in scss on homepage

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -2,9 +2,31 @@ import '../../styles/main.scss'
 
 export default function Home() {
   return (
-    <div className="home-main d-flex flex-column align-items-center justify-content-center text-center">
-      <h1 className='display-1'>Grail</h1>
-      <p className='h1'>Buy || Sell || Browse</p>
+    <div className="home-main">
+      <div className="d-flex flex-column align-items-center justify-content-center text-center">
+        <h1 className='display-1'>Grail</h1>
+        <p className='h1'>Buy || Sell || Browse</p>
+      </div>
+      <div className="carousel-container">
+        <div className="carousel-content">
+          <div className="carousel-item"><h2>Jordan</h2></div>
+          <div className="carousel-item"><h2>Adidas</h2></div>
+          <div className="carousel-item"><h2>Nike</h2></div>
+          <div className="carousel-item"><h2>Converse</h2></div>
+          <div className="carousel-item"><h2>New Balance</h2></div>
+          <div className="carousel-item"><h2>Asics</h2></div>
+          <div className="carousel-item"><h2>Reebok</h2></div>
+          <div className="carousel-item"><h2>Collabs</h2></div>
+          <div className="carousel-item"><h2>Jordan</h2></div>
+          <div className="carousel-item"><h2>Adidas</h2></div>
+          <div className="carousel-item"><h2>Nike</h2></div>
+          <div className="carousel-item"><h2>Converse</h2></div>
+          <div className="carousel-item"><h2>New Balance</h2></div>
+          <div className="carousel-item"><h2>Asics</h2></div>
+          <div className="carousel-item"><h2>Reebok</h2></div>
+          <div className="carousel-item"><h2>Collabs</h2></div>
+        </div>
+      </div>
     </div>
   )
 }
